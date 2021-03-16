9597c553ac83b3f28a38f6d14b2813fe9c0252ee
“Mrdman9202” <dannycunningham4@gmail.com>
2021-03-16 16:30:10 +0000

    express added

--- FitnessTracker/index.js
+++ FitnessTracker/index.js
@@ -1,10 +1,26 @@
+const express = require('express');
+const path = require('path');
+// const router = require('./routes/guestbookRoutes');
+// const mustache = require('mustache-express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+const public = path.join(__dirname, 'public');
+
+// app.engine('mustache', mustache());
+// app.set('view engine', 'mustache');
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(public));
+// app.use('/', router);
+
+
 app.use(function(req, res) {
     res.status(404);
     res.send('Oops! We didn\'t find what you are looking for.');
-   })
+})
    
    
-   app.listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
-   }) 
+}) 
    
